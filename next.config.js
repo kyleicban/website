f22edf56c9299c4a222782d16fd3765a18407dad
Kyle Icban <kicban29@gmail.com>
2026-01-17 14:50:18 -0800

    Updated next js config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ophdlafxdp1yk5hy.public.blob.vercel-storage.com',
+        pathname: '/photos/**', // Allow any path under /photos
+      },
+    ],
   },
 }
 
