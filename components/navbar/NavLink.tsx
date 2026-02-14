d75fa34bd7e1ed14b0c205f17db4d51cc9040b75
Kyle Icban <kicban29@gmail.com>
2026-02-13 22:21:09 -0800

    Remove focus for nav links

--- components/navbar/NavLink.tsx
+++ components/navbar/NavLink.tsx
@@ -10,7 +10,7 @@ export default function NavLink({
   return (
     <Link
       href={href}
-      className="text-sm text-neutral-600 dark:text-neutral-400 hover:text-neutral-900 dark:hover:text-neutral-100 transition-colors focus:outline-none focus:ring-2 focus:ring-neutral-500 rounded"
+      className="text-sm text-neutral-600 dark:text-neutral-400 hover:text-neutral-900 dark:hover:text-neutral-100 transition-colors rounded"
     >
       {children}
     </Link>
